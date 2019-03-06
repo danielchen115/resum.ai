8bd75c7d05167d19361b187358aadbc533372d6c
Daniel Chen <daniel.chen115@gmail.com>
2019-03-05 23:45:49 -0500

    first iteration

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, jsonify
+from werkzeug.utils import secure_filename
+from helper import *
+
+UPLOAD_FOLDER = '.'
+ALLOWED_EXTENSIONS = set(['pdf'])
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return None
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return None
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            suggestions = jsonify(get_suggestions(UPLOAD_FOLDER + '/' + filename))
+            os.remove(UPLOAD_FOLDER + '/' + filename)
+            return suggestions
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ helper.py
@@ -0,0 +1,77 @@
+import string
+import redis
+import scipy
+import collections
+from apyori import apriori
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import PyPDF2
+
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
+r.setnx("resume_id", 1)
+
+
+def parse_resume(file):
+    resume = open(file, 'rb')
+    reader = PyPDF2.PdfFileReader(resume)
+
+    page = reader.getPage(0).extractText()
+    punctuation = string.punctuation.replace("+", "")
+    stop_words = set(stopwords.words('english'))
+
+    table = str.maketrans({key: " " for key in punctuation})
+    page = set([x.lower() for x in word_tokenize(page.translate(table))])
+
+    words = set([x for x in page - stop_words if (not any(c.isdigit() for c in x) and len(x) > 1)])
+
+    return words
+
+
+def set_resume(words):
+    p = r.pipeline()
+    resume_id = r.get("resume_id")
+    p.sadd("resume:" + resume_id, *words)
+    p.incr("resume_id")
+    p.execute()
+
+
+def build_transaction_matrix():
+    resumes = r.keys('resume:*')
+    resume_words = []
+
+    for resume in resumes:
+        resume_words.append(list(r.smembers(resume)))
+
+    return resume_words
+
+
+def get_scores(transactions, resume_words):
+    associations = list(apriori(transactions, min_support=0.5, min_confidence=0.8))
+    suggestion_scores = collections.Counter()
+    for association in associations:
+        support = association[1]
+        for rule in association[2]:
+            base_items = rule[0]
+            new_item = list(rule[1])[0]
+            confidence = rule[2]
+            if not resume_words.intersection(base_items):
+                continue
+            if new_item in resume_words:
+                continue
+            suggestion_scores[new_item] += confidence * support
+
+    return suggestion_scores
+
+
+def get_suggestions(resume_file):
+    curr_resume = parse_resume(resume_file)
+    if len(curr_resume) < 10:
+        return {"ERROR: ": "Resume could not be parsed. This may be due to the font, or compression of the file."}
+    transactions = build_transaction_matrix()
+    scores = get_scores(transactions, curr_resume)
+    set_resume(curr_resume)
+
+    return list(scores.keys())
+
