21c654e0d4c6ef3324b3bdf06877e09c40b2dfb9
Daniel Chen <daniel.chen115@gmail.com>
2019-03-16 20:58:35 -0400

    switch to fp growth algorithm

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ import string
 import redis
 import scipy
 import collections
-from apyori import apriori
+import pyfpgrowth as fpg
 import pandas as pd
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -10,6 +10,7 @@ import PyPDF2
 
 
 r = redis.StrictRedis(host='ec2-3-208-118-12.compute-1.amazonaws.com', port=28069, password='pb07233ae908a5a1391478380668e68d52cfa102eb9a867f81c9c5dcdece9289c', db=0, charset="utf-8", decode_responses=True)
+# r = redis.StrictRedis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True)
 r.setnx("resume_id", 1)
 
 
@@ -49,23 +50,18 @@ def build_transaction_matrix():
 
     return resume_words
 
-
+# Scores are decided by the Rule Power Factor (confidence(A => B) * support(A))
 def get_scores(transactions, resume_words):
-    associations = list(apriori(transactions, min_support=0.5, min_confidence=0.8))
+    num_resumes = len(transactions)
     suggestion_scores = collections.Counter()
-    for association in associations:
-        support = association[1]
-        for rule in association[2]:
-            base_items = rule[0]
-            new_item = list(rule[1])[0]
-            confidence = rule[2]
-            if not resume_words.intersection(base_items):
-                continue
-            if new_item in resume_words:
-                continue
-            suggestion_scores[new_item] += confidence * support
+    patterns = fpg.find_frequent_patterns(transactions, num_resumes / 1.5)
+    rules = fpg.generate_association_rules(patterns, 0.5)
+    for antecedent, consequent in rules.items():
+        if set(antecedent).issubset(resume_words) and antecedent in patterns:
+            suggestion_scores[consequent[0]] += ((patterns[antecedent] * consequent[1]))
 
-    return suggestion_scores
+    suggestions = set.union(*[set(x) for x in suggestion_scores if suggestion_scores[x] >= 3])
+    return {x for x in suggestions if x not in resume_words}
 
 
 def get_suggestions(resume_file):
